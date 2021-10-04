5de26fccc7261b6a170ba3f792ab78be5f26d97f
Yuri Furtado Alcântara - YAL <yuri.alcantara@iteris.local>
2021-10-04 16:00:10 -0300

    configuração de externalização

---
+++ src/test/java/br/com/yuri/configserve/ConfigServeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yuri.configserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
